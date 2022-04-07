ad105d53bc7be4cea83ae6b6768308fd6fc65d93
Talita Bernardino <83371452+talibernardi@users.noreply.github.com>
2022-04-07 16:44:01 -0300

    modificacao

--- Calculos/Calculo.cs
+++ Calculos/Calculo.cs
@@ -4,25 +4,10 @@ namespace Calculos
 {
     public static class Calculo
     {
-        public static double Somar(double numero1, double numero2)
-        {
-            return numero1 + numero2;
-        }
-        public static double Subtrair(double numero1, double numero2)
-        {
-            return numero1 - numero2;
-        }
-        public static double Multiplicar(double numero1, double numero2)
-        {
-            return numero1 * numero2;
-        }
-        public static double Dividir(double numero1, double numero2)
-        {
-            return numero1 / numero2;
-        }
-        public static bool IsNumeroPar(int numero)
-        {
-            return numero % 2 == 0;
-        }
+        public static double Somar(double numero1, double numero2) => numero1 + numero2;
+        public static double Subtrair(double numero1, double numero2) => numero1 - numero2;
+        public static double Multiplicar(double numero1, double numero2) => numero1 * numero2;
+        public static double Dividir(double numero1, double numero2) => numero1 / numero2;
+        public static bool IsNumeroPar(int numero) => numero % 2 == 0;
     }
 }
--- NUCalculos/Calculadora.cs
+++ NUCalculos/Calculadora.cs
@@ -2,9 +2,9 @@
 {
     public class Calculadora
     {
-        public int Soma(int operador1, int operador2) => operador1 + operador2;
-        public int Subtracao(int operador1, int operador2) => operador1 - operador2;
-        public int Multiplicacao(int operador1, int operador2) => operador1 * operador2;
+        public int Soma(int numero1, int numero2) => numero1 + numero2;
+        public int Subtracao(int numero1, int numero2) => numero1 - numero2;
+        public int Multiplicacao(int numero1, int numero2) => numero1 * numero2;
         public int Divisao(int dividendo, int divisor) => dividendo / divisor;
         public (int quociente, int resto) RestoDivisao(int dividendo, int divisor) => (dividendo / divisor, dividendo % divisor);
     }
